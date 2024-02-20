912d15eb42f665128b3f8e9d828df23173fa1784
imelos <imelose@gmail.com>
2024-02-20 15:59:12 +0400

    +

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native";
-import { Props } from "../../App";
+import { Props } from "./../navigation/Navigation";
 
 const Login: React.FC<Props<"login">> = ({navigation}) => {
   return (
